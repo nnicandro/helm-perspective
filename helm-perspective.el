7276fcf0530b3e20cf8c35f56d7ac556177b4e53
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-12-02 01:56:27 -0600

    Don't rely on `with-perspective`

--- helm-perspective.el
+++ helm-perspective.el
@@ -6,28 +6,29 @@
 ;; TODO: Caching buffer lists
 
 (defun helm-persp-buffers-list--init ()
-  (let (persp-before-switch-hook
-        persp-switch-hook
-        persp-activated-hook
-        ;; prevent overriding the current value when calling
-        ;; `helm-buffers-list--init'
-        helm-buffers-list-cache
-        ;; disable these since `perspective' activates buffers which results in
-        ;; lots of calls to buffer display functions.
-        window-configuration-change-hook
-        purpose--active-p
-        golden-ratio-mode)
-    (with-perspective (helm-attr 'name)
-      (helm-buffers-list--init)
-      (helm-attrset 'persp-buffers helm-buffers-list-cache))))
+  ;; Adapted from `helm-buffers-list--init'
+  (helm-attrset
+   'candidates (cl-loop
+                for buf in (persp-buffers
+                            (gethash (helm-attr 'name) perspectives-hash))
+                for name = (buffer-name buf)
+                if (not (null name)) collect name))
+  (let ((result (cl-loop for b in (helm-attr 'candidates)
+                         maximize (length b) into len-buf
+                         maximize (length (with-current-buffer b
+                                            (format-mode-line mode-name)))
+                         into len-mode
+                         finally return (cons len-buf len-mode))))
+    (unless (default-value 'helm-buffer-max-length)
+      (helm-set-local-variable 'helm-buffer-max-length (car result)))
+    (unless (default-value 'helm-buffer-max-len-mode)
+      (helm-set-local-variable 'helm-buffer-max-len-mode (cdr result)))))
 
-(defun helm-persp-buffers-list ()
-  (helm-attr 'persp-buffers))
-
-;; TODO: More actions
+;; TODO: The buffer actions for `helm-source-buffers' are still available.
+;; Merge these actions into those?
 (defconst helm-persp-actions
   (helm-make-actions
-   "Switch to buffer in its perspective"
+   "Switch to buffer in perspective"
    (lambda (candidate)
      (persp-switch (helm-attr 'name))
      (switch-to-buffer candidate))
@@ -40,14 +41,19 @@
 
 ;;;###autoload
 (defun helm-persp-buffers ()
+  "Pre-configured `helm' for `perspective'."
   (interactive)
   (helm
    :sources (cl-loop
-             for name in (hash-table-keys perspectives-hash)
+             for name in
+             (sort (hash-table-keys perspectives-hash)
+                   (lambda (a b)
+                     (let ((c (compare-strings a nil nil b nil nil t)))
+                       (or (eq c t) (< c 0)))))
              for persp = (gethash name perspectives-hash)
              for src = (helm-make-source name 'helm-source-buffers
                          :init 'helm-persp-buffers-list--init
-                         :candidates 'helm-persp-buffers-list)
+                         :keymap helm-buffer-map)
              do (setf (alist-get 'action src) helm-persp-actions)
              and collect src)
    :truncate-lines helm-buffers-truncate-lines
