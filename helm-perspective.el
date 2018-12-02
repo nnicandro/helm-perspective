eaf417b14dfa4e91c9b6bd1ce64e27376144f360
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-12-02 01:56:53 -0600

    Add header

--- helm-perspective.el
+++ helm-perspective.el
@@ -1,5 +1,40 @@
+;;; helm-perspective.el --- Helm integration with perspective -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 18 Feb 2018
+;; Version: 0.0.1
+;; X-URL: https://github.com/nathan/helm-perspective
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Show the buffers in every `perspective' of the current frame, each
+;; perspective is shown as a `helm' source.
+
+;;; Code:
+
 (require 'perspective)
 (require 'helm-buffers)
+
+(defgroup helm-perspective nil
+  "Helm integration with perspective"
+  :group 'helm)
 
 (declare-function hash-table-keys "subr-x" (hash-table))
 
@@ -74,3 +109,5 @@
      :buffer "*helm-persp-buffers*")))
 
 (provide 'helm-perspective)
+
+;;; helm-perspective.el ends here
